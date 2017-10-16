e959b4f3b8cb92df7ae3b760972f8f7f19f63341
Kabir Cahill <kcahill4@my.bcit.ca>
2017-10-15 19:57:11 -0700

    New registered users are now given the role of Member.

--- ZenithWebSite/Controllers/AccountController.cs
+++ ZenithWebSite/Controllers/AccountController.cs
@@ -156,6 +156,9 @@ namespace ZenithWebSite.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    var newUser = UserManager.FindByEmail(user.Email);
+                    UserManager.AddToRole(newUser.Id, "Member");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
